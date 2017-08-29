90b89ceff8ce0f56d25d1cfb3b476cb83e86e2aa
Ryan Leary <ryan@qntfy.com>
2017-08-29 12:06:01 -0400

    Make python3 compatible, add cpu support

--- pmet/__main__.py
+++ pmet/__main__.py
@@ -32,6 +32,7 @@ def parse_args():
     parser.add_argument('--bidirectional', action='store_true')
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--log-interval', type=int, default=100)
+    parser.add_argument('--cuda', action='store_true')
     return parser.parse_args()
 
 # --
@@ -41,13 +42,13 @@ def train_epoch(model, train_iter, opt, decay=0.9):
     _ = model.train()
     acc = 0.0
     for batch_idx, batch in tqdm(enumerate(train_iter), total=len(train_iter)):
-        
+
         # Prep data
         x, y = batch.val, batch.lab.squeeze()
-        
+
         # Train step
         _, _ = model.train_step(x, y, opt)
-        
+
         # !! Add logging
 
 
@@ -56,9 +57,9 @@ def test(model, test_iter):
     for batch_idx, batch in tqdm(enumerate(test_iter), total=len(test_iter)):
         x, y = batch.val, batch.lab.squeeze()
         score = model(x)
-        
+
         pred = score.max(1)[1].data.squeeze()[0]
-        
+
         yield {
             "pred" : pred,
             "act" : y.data.squeeze()[0],
@@ -68,47 +69,50 @@ def test(model, test_iter):
 # Evaluation
 
 if __name__ == "__main__":
-    
+
     args = parse_args()
-    
+
     # Model definition
     if args.attention and not args.bidirectional:
         raise Exception('if attention=1, bidirectional=1')
-    
+
     # IO
     (train_iter, test_iter), (n_chars, n_classes) = make_iter(args.data_dir)
-    
-    print >> sys.stderr, 'initializing character-level LSTM | attention=%d | bidirectional=%d' %\
-        (args.attention, args.bidirectional)
-    
+
+    print('initializing character-level LSTM | attention=%d | bidirectional=%d' %\
+        (args.attention, args.bidirectional), file=sys.stderr)
+
     if args.attention:
-        
+
         model = AttCharLSTM(**{
             "n_chars"    : n_chars,
             "n_classes"  : n_classes,
-            
+
             "att_channels"   : 5, # !! Allow these to be passed via CLI
             "att_dim"        : 16,
-            "emb_dim"        : 64, 
+            "emb_dim"        : 64,
             "rec_hidden_dim" : 32,
-        }).cuda()
+        })
     else:
         model = CharLSTM(**{
             "n_chars"    : n_chars,
             "n_classes"  : n_classes,
-            
+
             "bidirectional"  : args.bidirectional,
             "emb_dim"        : 64,
             "rec_hidden_dim" : 32,
-        }).cuda()
-    
-    
+        })
+
+    if args.cuda:
+        model.cuda()
+
+
     opt = torch.optim.Adam(model.parameters())
-    
+
     for epoch in range(args.epochs):
-        print >> sys.stderr, 'epoch=%d | train' % epoch
+        print('epoch=%d | train' % epoch, file=sys.stderr)
         _ = train_epoch(model, train_iter, opt)
-        
-        print >> sys.stderr, 'epoch=%d | test' % epoch
+
+        print('epoch=%d | test' % epoch, file=sys.stderr)
         preds = pd.DataFrame(list(test(model, test_iter)))
-        print >> sys.stderr, "acc=%f\n--" % (preds.act == preds.pred).mean()
+        print("acc=%f\n--" % (preds.act == preds.pred).mean(), file=sys.stderr)
--- pmet/data.py
+++ pmet/data.py
@@ -5,28 +5,29 @@
 
 from torchtext import data
 
-def make_iter(indir, train='train.tsv', test='test.tsv', validation=None, batch_size=32):
-    
+def make_iter(indir, train='train.tsv', test='test.tsv', validation=None, batch_size=32, device=None):
+
     LABS = data.Field()
     VALS = data.Field(tokenize=list)
-    
-    datasets = data.TabularDataset.splits(path=indir, 
+
+    datasets = data.TabularDataset.splits(path=indir,
         train=train,
         test=test,
         validation=validation,
-        format='tsv', 
+        format='tsv',
         fields=[
             ("lab", LABS),
             ("val", VALS),
         ])
-    
+
     LABS.build_vocab(datasets[0])
     VALS.build_vocab(datasets[0])
-    
+
     return data.BucketIterator.splits(
         datasets,
         batch_size=1,
         shuffle=True,
         repeat=False,
+        sort_key=lambda x: len(x.val),
+        device=device
     ), (len(VALS.vocab), len(LABS.vocab))
-
